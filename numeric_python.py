d3a2e0f6a3cf630ac628128e8268a0e72dda2601
Debojit Saha <debojits76@gmail.com>
2026-01-21 23:23:37 +0530

    numpy day 1

---
+++ numeric_python.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1,2,3,4,5,6]) #1dim
+b = np.array([[1,2,4,6],[3,4,9,7]]) #2 dim
+c=  np.array([[[1,3],[3,4]],[[4,5],[6,7]]]) #3 dim
+
+#find the dimension
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+
+#find the element using indexing
+print(f"4th element of 2nd row of array b:{b[1,3]}")
+
+#Array slicing  [start:end:step].
+print(b[1:])
+print(a[0:4:2])
